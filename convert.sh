3b47b98dbbac2d70e0649bbef446f27d4ff04bc2
Mohammad Reza <mohammad.chavoshipor@gmail.com>
2020-04-15 22:51:58 +0430

    update #fix

--- convert.sh
+++ convert.sh
@@ -1,1 +1,2 @@
-LIST = 
+# for file in ./edx-platform/locale/*/*/*.po; do msgfmt $file -o $(dirname file)"/"$(basename ${file%.*}".mo"); done;
+for file in ./edx-platform/locale/*/*/*.po; do msgfmt $file -o $(dirname $file)/$(basename ${file%.*}).mo; done;
